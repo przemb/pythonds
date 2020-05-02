ce2beee6e6249e0535b9d0428ded20a2ac612653
przemb <sendthenote@gmail.com>
2020-05-02 18:42:14 +0200

    trees - parse_tree added

---
+++ trees_and_tree_algorithms/parse_tree.py
@@ -0,0 +1,72 @@
+from nose.tools import assert_equal, assert_raises
+from binary_tree import BinaryTree
+
+# parse example input: (3 + (4 * 5))
+# and save it as a tree
+
+# algorithm - rules:
+# 0. create root
+# 1. if '(' - add add new node as a left child, move down, set current to left child
+# 2. if number - fill current node and move up
+# 3. if operator [-, +, /...] - fill current node, create right child, move down, set current to right child
+# 4. if ')' - move up - current = parent
+
+
+class ParseTree:
+    def __init__(self, input_str):
+        self.raw_input = input_str
+        self.parsed_input = None
+        self.tree = BinaryTree(None)
+        self.parent_stack = []  # pseudo stack to keep track of current and parent
+        self.current = self.tree
+
+    def _parse_input(self):
+        tmp = self.raw_input.replace(" ", "")
+        self.parsed_input = list(tmp)
+
+    def _process_left(self):
+        self.current.insert_left(None)
+        self.parent_stack.append(self.current)
+        self.current = self.current.get_left_child()
+
+    def _process_right(self):
+        try:
+            self.current = self.parent_stack.pop()
+        except IndexError:
+            pass
+
+    def _process_operator(self, c):
+        self.current.set_root_value(c)
+        self.current.insert_right(None)
+        self.parent_stack.append(self.current)
+        self.current = self.current.get_right_child()
+
+    def _process_number(self, c):
+            self.current.set_root_value(int(c))
+            self.current = self.parent_stack.pop()
+
+    def run(self):
+        self._parse_input()
+        for c in self.parsed_input:
+            if c == '(':
+                self._process_left()
+            elif c == ')':
+                self._process_right()
+            elif c in ['+', '-', '*', '/']:
+                self._process_operator(c)
+            else:
+                self._process_number(c)
+
+
+p = ParseTree("(3 + (4 * 5))")
+p._parse_input()
+assert_equal(['(', '3', '+', '(', '4', '*', '5', ')', ')'], p.parsed_input)
+
+p.run()
+assert_equal('+', p.tree.root)
+assert_equal(3, p.tree.get_left_child().root)
+assert_equal('*', p.tree.get_right_child().root)
+
+p = ParseTree("((7 + 3) * (5 - 2))")
+p.run()
+
