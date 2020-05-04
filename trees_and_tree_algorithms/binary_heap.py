e1fefbe5b00a39f9f7d8f423d52afc90ca1f5dee
przemb <sendthenote@gmail.com>
2020-05-05 00:45:22 +0200

    trees - binary heap - _percolate_down() fixed, build_tree improved, tests added

--- trees_and_tree_algorithms/binary_heap.py
+++ trees_and_tree_algorithms/binary_heap.py
@@ -48,20 +48,19 @@ class BinaryHeap:                   # p - parent, 2p - left child, 2p + 1 - righ
 
     def _percolate_down(self, item_idx):
         n = item_idx
-        elem = self.heap_list[n]
-        while n < (self.heap_size - 1):
+        while n <= self.heap_size//2:  # elems > self.heap_size are leaves, do not analyse them!
+            elem = self.heap_list[n]
             child_idx = self._get_min_child_idx(n)
             if elem > self.heap_list[child_idx]:
                 self.heap_list[n], self.heap_list[child_idx] = self.heap_list[child_idx], self.heap_list[n]
-            n = n * 2
+            n = child_idx  # n is now chosen child, go one level down
 
     def _get_min_child_idx(self, n):
         """
         :param n: node index
         """
-        left = self.heap_list[2*n]
         if 2*n + 1 > self.size():
-            return left
+            return 2*n  # return left child idx
         else:
             if self.heap_list[2*n] < self.heap_list[2*n+1]:
                 return 2*n
@@ -78,25 +77,38 @@ class BinaryHeap:                   # p - parent, 2p - left child, 2p + 1 - righ
         """Build tree using given list_of_keys"""
         self.heap_list += list_of_keys
         self.heap_size += len(list_of_keys)
+        i = len(list_of_keys) // 2  # all elems beyond the halfway are leaves
+        while i > 0:
+            self._percolate_down(i)  # start with first leaf
+            i = i - 1
 
 
 def test_insert():
     b = BinaryHeap()
     b.build_heap([5, 9, 11, 14, 18, 19, 21, 33, 17, 27])
+    assert_equal(10, b.size())
+
     b.insert(7)
     assert_equal([0, 5, 7, 11, 14, 9, 19, 21, 33, 17, 27, 18], b.heap_list)
+    assert_equal(11, b.size())
 
     b = BinaryHeap()
+    assert_equal(0, b.size())
     b.insert(7)
+
     assert_equal([0, 7], b.heap_list)
+    assert_equal(1, b.size())
 
 
 def test_del_min():
     b = BinaryHeap()
     b.build_heap([5, 9, 11, 14, 18, 19, 21, 33, 17, 27])
+    assert_equal(10, b.size())
+
     tmp = b.del_min()
     assert_equal(5, tmp)
     assert_equal([0, 9, 14, 11, 17, 18, 19, 21, 33, 27], b.heap_list)
+    assert_equal(9, b.size())
 
     b = BinaryHeap()
     b.build_heap([5])
@@ -106,6 +118,29 @@ def test_del_min():
     assert_true(b.is_empty())
 
 
+def test_build_heap():
+    b = BinaryHeap()
+    b.build_heap([9, 6, 5, 2, 3])
+    assert_equal(5, b.size())
+    assert_equal([0, 2, 3, 5, 6, 9], b.heap_list)
+
+    b = BinaryHeap()
+    b.build_heap([5])
+    assert_equal(1, b.size())
+    assert_equal([0, 5], b.heap_list)
+
+    b = BinaryHeap()
+    b.build_heap([5, 9, 11, 14, 18, 19, 21, 33, 17, 27])
+    assert_equal(10, b.size())
+    assert_equal([0, 5, 9, 11, 14, 18, 19, 21, 33, 17, 27], b.heap_list)
+
+    b = BinaryHeap()
+    b.build_heap([9, 3, 7, 2, 6])
+    assert_equal(5, b.size())
+    assert_equal([0, 2, 3, 7, 9, 6], b.heap_list)
+
+
 if __name__ == "__main__":
     test_insert()
     test_del_min()
+    test_build_heap()
