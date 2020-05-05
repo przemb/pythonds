f9d4e89becd9b0f6bf791849efcb68c99ad9b048
przemb <sendthenote@gmail.com>
2020-05-05 11:54:25 +0200

    trees - binary search tree - skeleton of class

--- trees_and_tree_algorithms/binary_search_tree.py
+++ trees_and_tree_algorithms/binary_search_tree.py
@@ -6,3 +6,28 @@ from nose.tools import assert_equal
 # 2. search on hash table
 # 3. binary search on trees  <------
 
+
+class BinarySearchTree:
+    def __init__(self):
+        pass
+
+    def __setitem__(self, key, value):
+        self.put(key, value)
+
+    def __getitem__(self, item):
+        self.get(item)
+
+    def __delitem__(self, key):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __contains__(self, item):
+        pass
+
+    def put(self, key, value):
+        pass
+
+    def get(self, key):
+        pass
