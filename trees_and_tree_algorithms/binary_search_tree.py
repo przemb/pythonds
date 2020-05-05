a55a1d1c7630c6fd475b316405c293d46ed789be
przemb <sendthenote@gmail.com>
2020-05-05 13:12:25 +0200

    trees - binary search tree - TreeNode class added

--- trees_and_tree_algorithms/binary_search_tree.py
+++ trees_and_tree_algorithms/binary_search_tree.py
@@ -5,11 +5,13 @@ from nose.tools import assert_equal
 # 1. binary search on list
 # 2. search on hash table
 # 3. binary search on trees  <------
+# BST property: keys less then parent are in left subtree, key greater than parent are in right subtree
 
 
 class BinarySearchTree:
     def __init__(self):
-        pass
+        self.root = None
+        self.size = 0
 
     def __setitem__(self, key, value):
         self.put(key, value)
@@ -21,9 +23,12 @@ class BinarySearchTree:
         pass
 
     def __len__(self):
-        pass
+        return self.size
 
     def __contains__(self, item):
+        pass
+
+    def __iter__(self):
         pass
 
     def put(self, key, value):
@@ -31,3 +36,43 @@ class BinarySearchTree:
 
     def get(self, key):
         pass
+
+
+class TreeNode:
+    def __init__(self, key, value, left_child=None, right_child=None, parent=None):
+        self.key = key
+        self.value = value
+        self.left_child = left_child
+        self.right_child = right_child
+        self.parent = parent
+
+    def has_left_child(self):
+        return self.left_child
+
+    def has_right_child(self):
+        return self.right_child
+
+    def is_left_child(self):
+        return self.parent and self.parent.left_child == self
+
+    def is_right_child(self):
+        return self.parent and self.parent.right_child == self
+
+    def is_root(self):
+        return self.parent is None
+
+    def is_leaf(self):
+        return self.left_child is None and self.right_child is None
+
+    def has_any_children(self):
+        return self.left_child or self.right_child
+
+    def update_node_data(self, key, value, left_child, right_child):
+        self.key = key
+        self.value = value
+        if self.has_left_child():
+            self.left_child = left_child
+            self.left_child.parent = self
+        if self.has_right_child():
+            self.right_child = right_child
+            self.right_child.parent = self
